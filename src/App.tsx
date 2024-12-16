9884ed578972637c1df7ee77236270e0814d9f5a
MaaRadha <Omyoga2016@outlook.com>
2024-12-16 15:36:43 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-7xl font-bold underline text-red-600">
+        Simple React Typescript Tailwind Sample
+      </h1>
+    </div>
+  );
+};
+
+export default App;
