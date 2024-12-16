c13d41ce97dfcc78841ba5948cb980f832c41c97
MaaRadha <Omyoga2016@outlook.com>
2024-12-17 00:39:31 +0100

    icons helper file

---
+++ src/helpers/Icons.tsx
@@ -0,0 +1,39 @@
+import archicadIcons from "../assets/icons/archicad.png";
+import autocadIcons from "../assets/icons/autocad.png";
+import revitIcons from "../assets/icons/revit.png";
+import solibriIcons from "../assets/icons/solibri.png";
+import enscapeIcons from "../assets/icons/enscape.png";
+import twinmotion from "../assets/icons/twinmotion.png";
+
+export const icons = [
+  {
+    Id: 1,
+    pic: archicadIcons,
+    name: "Archicad",
+  },
+  {
+    Id: 2,
+    pic: autocadIcons,
+    name: "Autocad",
+  },
+  {
+    Id: 3,
+    pic: revitIcons,
+    name: "Revit",
+  },
+  {
+    Id: 4,
+    pic: solibriIcons,
+    name: "Solibri",
+  },
+  {
+    Id: 5,
+    pic: enscapeIcons,
+    name: "enscape",
+  },
+  {
+    Id: 6,
+    pic: twinmotion,
+    name: "twinmotion",
+  },
+];
