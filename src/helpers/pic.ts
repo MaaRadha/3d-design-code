5935f3c8d7210987496e41766405e1d8a86adbdf
MaaRadha <Omyoga2016@outlook.com>
2024-12-17 13:18:32 +0100

    rename and edit file

---
+++ src/helpers/pic.ts
@@ -0,0 +1,40 @@
+import frame from "../../assets/frame_1.jpg";
+import pool from "../../assets/basseng_1.jpg";
+import bolerBad from "../../assets/bolerbad.jpg";
+import freelanc from "../../assets/freelance.jpg";
+import cabin from "../../assets/freelanc_cabin.jpg";
+import ski from "../../assets/ski-min.jpg";
+import bygg from "../../assets/bygg.png";
+
+export const pictures = [
+  {
+    id: 1,
+    pic: frame,
+    name: "frame",
+  },
+  {
+    id: 2,
+    pic: pool,
+    name: "pool",
+  },
+  {
+    id: 3,
+    pic: bolerBad,
+    name: "bolerbad",
+  },
+  {
+    id: 4,
+    pic: freelanc,
+    name: "freelancer",
+  },
+  {
+    id: 5,
+    pic: cabin,
+    name: "cabin",
+  },
+  {
+    id: 6,
+    pic: ski,
+    name: "ski",
+  },
+];
