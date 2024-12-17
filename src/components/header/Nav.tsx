e2ecafe3ebd8c29065db584bcb0c9d2093af25b2
MaaRadha <Omyoga2016@outlook.com>
2024-12-17 15:41:17 +0100

    edit nav

--- src/components/header/Nav.tsx
+++ src/components/header/Nav.tsx
@@ -1,28 +1,32 @@
 import logo from "/logo_home.png";
 import avtarPic from "../../assets/avatar.jpg";
 import { Tooltip } from "react-tooltip";
+
 const Nav = () => {
   return (
-    <div className="max-w-5xl mx-auto container p-4 mb-4 flex justify-between items-center ">
+    <div className="max-w-5xl mx-auto container p-4 mb-4 flex justify-between items-center">
+      {/* Logo Section */}
       <div className="flex flex-col items-center">
         <img
           src={logo}
           alt="logo"
-          className="w-full max-w-16 rounded-lg p-1 backdrop-blur-lg border-2 border-gray-100 "
+          className="w-full max-w-16 rounded-lg p-1 backdrop-blur-lg border-2 border-gray-100"
         />
         <span className="text-sm px-2">Alt fra 3D-design til kode.</span>
       </div>
-      <div id="not-clickable">
+
+      {/* Avatar with Tooltip */}
+      <div id="not-clickable" className="relative">
         <img
           src={avtarPic}
-          alt="my profile img "
+          alt="my profile img"
           className="w-12 rounded-full cursor-pointer"
         />
         <Tooltip
-          className=""
           anchorSelect="#not-clickable"
           place="bottom"
-          content="Hei 😊! Jeg heter Rohit. Se mer informasjon i footer. Jeg har kompetanse innen design og utvikling. Send meg gjerne en e-post eller ring!"
+          className="bg-gray-800 text-white text-sm rounded-lg p-2 shadow-lg"
+          content="Hei 😊! Jeg heter Rohit. Se mer informasjon i footer!"
         />
       </div>
     </div>
