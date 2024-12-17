c1e36d1d54ef4a102ea8eadff8301163ab49f5e8
MaaRadha <Omyoga2016@outlook.com>
2024-12-17 14:17:11 +0100

    added pic

--- src/helpers/pic.ts
+++ src/helpers/pic.ts
@@ -1,40 +1,45 @@
-import frame from "../../assets/frame_1.jpg";
-import pool from "../../assets/basseng_1.jpg";
-import bolerBad from "../../assets/bolerbad.jpg";
-import freelanc from "../../assets/freelance.jpg";
-import cabin from "../../assets/freelanc_cabin.jpg";
-import ski from "../../assets/ski-min.jpg";
-import bygg from "../../assets/bygg.png";
+import frame from "../assets/frame_1.jpg";
+import pool from "../assets/basseng_1.jpg";
+import bolerBad from "../assets/bolerbad.jpg";
+import freelanc from "../assets/freelance.jpg";
+import cabin from "../assets/freelanc_cabin.jpg";
+import ski from "../assets/ski-min.jpg";
+import nanestadModel from "../assets/nanestad_model-min.jpg";
 
 export const pictures = [
   {
     id: 1,
     pic: frame,
-    name: "frame",
+    name: "Proskjt hotel bygg",
   },
   {
     id: 2,
     pic: pool,
-    name: "pool",
+    name: "basseng",
   },
   {
     id: 3,
     pic: bolerBad,
-    name: "bolerbad",
+    name: "Bøler Bad",
   },
   {
     id: 4,
     pic: freelanc,
-    name: "freelancer",
+    name: "Frilance hytte projekt",
   },
   {
     id: 5,
     pic: cabin,
-    name: "cabin",
+    name: "Frilance hytte projekt",
   },
   {
     id: 6,
     pic: ski,
-    name: "ski",
+    name: "Ski bygg projekt",
+  },
+  {
+    id: 7,
+    pic: nanestadModel,
+    name: "Nanestad VGS bygg projekt",
   },
 ];
