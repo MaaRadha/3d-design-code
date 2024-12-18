ca13fb3adbbe3e68357bcfad2b7ef7ad0dad5c22
MaaRadha <Omyoga2016@outlook.com>
2024-12-18 12:37:06 +0100

    update text

--- src/helpers/pic.ts
+++ src/helpers/pic.ts
@@ -10,36 +10,36 @@ export const pictures = [
   {
     id: 1,
     pic: frame,
-    name: "Proskjt hotel bygg",
+    name: "Jeg jobbet i arkitektfirmaet Enerhaugen i Oslo, og jeg ble involvert i et prosjekt som hadde pågått lenge. Jeg fikk muligheten til å lage 3D-visualiseringene.",
   },
   {
     id: 2,
     pic: pool,
-    name: "basseng",
+    name: "Jeg jobbet i arkitektfirmaet Enerhaugen i Oslo, og jeg ble involvert i et sokkelprosjekt der jeg laget noen rendering av svømmebasseng med Enscape og Archicad.",
   },
   {
     id: 3,
     pic: bolerBad,
-    name: "Bøler Bad",
+    name: "Jeg jobbet i arkitektfirmaet Enerhaugen i Oslo, og jeg ble involvert i et Bøler Bad-prosjekt. Jeg lærte mye sammen med teamet. Bøler Bad ligger i Oslo, nær Bøler T-bane.",
   },
   {
     id: 4,
     pic: freelanc,
-    name: "Frilance hytte projekt",
+    name: "Under koronapandemien mistet jeg jobben som BIM-tekniker, men jeg var heldig som fikk noen frilansprosjekter. Jeg jobbet på et prosjekt med Archicad og brukte Twinmotion. Det var et sommerhytte- og campingprosjekt.",
   },
   {
     id: 5,
     pic: cabin,
-    name: "Frilance hytte projekt",
+    name: "Dette er en av hyttene jeg laget for en kunde som et frilansprosjekt.",
   },
   {
     id: 6,
     pic: ski,
-    name: "Ski bygg projekt",
+    name: "I arkitektfirmaet Spinn i Oslo jobbet jeg i over to år. Jeg var involvert i mange forskjellige prosjekter, inkludert konkurranser, som Ski-prosjektet.",
   },
   {
     id: 7,
     pic: nanestadModel,
-    name: "Nanestad VGS bygg projekt",
+    name: "På Nannestad vgs-prosjektet var min rolle å oppdatere tegningene av den eksisterende skolen. Jeg laget også hvitmodell-renderinger.",
   },
 ];
