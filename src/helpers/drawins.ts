f8de5d7107632b3c320e1e1bd745c8bbb036b285
MaaRadha <Omyoga2016@outlook.com>
2024-12-19 14:51:07 +0100

    addded componts

---
+++ src/helpers/drawins.ts
@@ -0,0 +1,33 @@
+import hytta from "../assets/drawings/hytte_aanesen.jpg";
+import hytta_1 from "../assets/drawings/hytte_aanesen_1.jpg";
+import draw from "../assets/drawings/drawing_1-min.jpg";
+import brenna from "../assets/drawings/brennahouse-min.jpg";
+import brannaSnit from "../assets/drawings/brenna3d-min.jpg";
+
+export const drawings = [
+  {
+    src: hytta,
+    id: 1,
+    alt: "2d tegning av Hytte Aanesen,  jeg ble involvert i et prosjekt som hadde pågått lenge.",
+  },
+  {
+    src: hytta_1,
+    id: 2,
+    alt: "2d fasade tegning av Hytte Aanesen.",
+  },
+  {
+    src: draw,
+    id: 3,
+    alt: "2d tegning av et bygg.",
+  },
+  //   {
+  //     src: brenna,
+  //     id: 4,
+  //     alt: "snitt tegning 2d av et hus brenna.",
+  //   },
+  //   {
+  //     src: brannaSnit,
+  //     id: 5,
+  //     alt: "snitt tegning 2d av et hus brenna. og 3d tegning av et hus brenna.",
+  //   },
+];
